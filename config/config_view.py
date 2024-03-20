3d79773208ef0d5654038fd19147461d44ae3082
samiro <sarkisovmichail007@gmail.com>
2024-03-20 19:59:18 +0300

    deletion nets relate with config

--- config/config_view.py
+++ config/config_view.py
@@ -2,6 +2,7 @@
 from fastapi import APIRouter, Depends
 import database
 from . import models, schemas
+from nets.models import Net
 
 router = APIRouter(tags=['config'], prefix='/config')
 
@@ -23,7 +24,6 @@ def get_config(db = Depends(database.get_session)):
 
 @router.post('')
 def add_config(body: schemas.Config, db = Depends(database.get_session)):
-  print(dict(body))
   for config_item in dict(body):
     if dict(body)[config_item] != None:
       new_config_item = config_models[config_item](**{config_item: dict(body)[config_item]})
@@ -33,8 +33,23 @@ def add_config(body: schemas.Config, db = Depends(database.get_session)):
   return new_config_item
 
 @router.delete('/{type}/{id}')
-def delete_config(type: str, id: int, db = Depends(database.get_session)):
-  db.query(config_models[type]).filter(config_models[type].id == id).delete()
-  db.commit()
-  return True
+def delete_config(type: str, id: int, force: bool | None = None, db = Depends(database.get_session)):
+  def nets_relate_with_config():
+    delete_element = db.query(config_models[type]).filter(config_models[type].id == id).first()
+    existing_nets = db.query(Net).filter(getattr(Net, type) == delete_element.id).all()
+    return existing_nets
+
+  current_nets = nets_relate_with_config()
+
+  if len(current_nets) == 0 and not force:
+    db.query(config_models[type]).filter(config_models[type].id == id).delete()
+    db.commit()
+    return True
+  elif force:
+    db.query(config_models[type]).filter(config_models[type].id == id).delete()
+    for net in current_nets:
+      db.delete(net)
+      db.commit()
+  else:
+    return False
 
